980255a7fa81150174e643d68c71c4717014f1b4
Brayan Medina <brayan5562@gmail.com>
2024-12-08 20:37:55 -0500

    📝 README changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CouchBaseAdapterModule } from './couch-base-adapter/couch-base-adapter.module';
+import { CompanyModule } from './company/company.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [CouchBaseAdapterModule, CompanyModule,ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
